b900b3f0bb2453db1de164199e1a1304bb7109da
David Hamp-Gonsalves <davidhampgonsalves@gmail.com>
2015-06-11 10:49:39 -0700

    adding osx binary for homebrew install

--- slackline.rb
+++ slackline.rb
@@ -2,18 +2,10 @@ class Slackline < Formula
   desc "Post updates/messages to slack with minimal disruption from the cmd line."
   homepage "https://github.com/davidhampgonsalves/slackline"
   url "https://github.com/davidhampgonsalves/slackline/archive/1.0.tar.gz"
-https://github.com/wincent/clipper/archive/0.2.tar.gz
   version "1.0"
 
-  depends_on "go" => :build
-  depends_on "gpm" => :build
-
   def install
-    ENV["GOPATH"] = buildpath
-    system "gpm", "install"
-    system "go", "build", "slackline.go"
-
-    bin.install "slackline"
+    bin.install "slackline-osx" => "slackline"
   end
 
   test do
