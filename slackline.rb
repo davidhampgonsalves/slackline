9945d801a3084c375fa5e6add614b8e30b723137
David Hamp-Gonsalves <davidhampgonsalves@gmail.com>
2015-06-12 06:59:24 -0700

    fixed homebrew permissions bug

--- slackline.rb
+++ slackline.rb
@@ -1,11 +1,11 @@
 class Slackline < Formula
   desc "Post updates/messages to slack with minimal disruption from the cmd line."
   homepage "https://github.com/davidhampgonsalves/slackline"
-  url "https://github.com/davidhampgonsalves/slackline/archive/1.0.tar.gz"
+  url "https://github.com/davidhampgonsalves/slackline/releases/download/1.0/slackline"
   version "1.0"
 
   def install
-    bin.install "slackline-osx" => "slackline"
+    bin.install "slackline"
   end
 
   test do
